bc2a951c2f54886f38bb1ba013228fdea43db47a
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-01-22 16:50:30 +0530

    leap year
    leap year or not

---
+++ leap year.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int yr;
+printf("\n enter the yaer:");
+scanf("%d",&yr);
+if(yr%4==0)
+printf("\n the given year is a leap year");
+else
+printf("\n the given year is not a leap year"0;
+getch();
+}
