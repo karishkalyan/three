5bd6bab6470e5d3c6e656893160253addef97e31
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-01-22 16:13:46 +0530

    even
    odd or even

---
+++ even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("\n enter a number:");
+scanf("%d",&a);
+if(a%2==0)
+printf("\n rhe number is even");
+else
+printf("\n the number is odd");
+getch();
+}
