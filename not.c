1649c085b20c9254e294ed745d486241a6c042ec
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-01-22 16:43:04 +0530

    alphabet
    code

---
+++ not.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+char fv;
+printf("\n enter a character:");
+scanf("%c",&fv);
+if((fv=='a')&&(fv=='z')||((fv=='A')&&(fv=='Z')))
+print("\n it is an alphabet");
+else
+printf("\n it is not an alphabet");
+getch();
+}
+
